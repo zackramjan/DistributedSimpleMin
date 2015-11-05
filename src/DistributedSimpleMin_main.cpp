413e40fa7f1237aa9f5f7ab7dab6b10050f26712
zack <zack@192.168.1.102>
2015-11-04 23:01:09 -0500

    added comments

--- src/DistributedSimpleMin_main.cpp
+++ src/DistributedSimpleMin_main.cpp
@@ -1,9 +1,9 @@
 //============================================================================
-// Name        : ForceMinRedis.cpp
-// Author      : 
+// Name        : DistributedSimpleMin_main.cpp
+// Author      :  Zack Ramjan
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : hazelcast aware distributed random min
 //============================================================================
 
 #include <iostream>
@@ -23,8 +23,8 @@ int main(int argc, char ** argv) {
 	}
 
 	//read in the system coords from file
-	PointList system;
-	system.fromFile(argv[1]);
+	PointList system; //my system
+	system.fromFile(argv[1]); //intially populate the system from file
 
 	//create the connection to a hazelcast server node.
 	//We want the Keys:Values to be String:String in this case
@@ -36,9 +36,9 @@ int main(int argc, char ** argv) {
 	cout << "this process has workerID=" << machineID <<endl;
 
 	float prevTotalEnergy = system.totalEnergy;
-	int badStreak = 0;
-	float badStreakMax = 10000.0;
-	float badStreakThrottle = 1.0;
+	int badStreak = 0; //my counter for failed trials
+	float badStreakMax = 10000.0; //how many failed trials I accept before taking another workers state
+	float badStreakThrottle = 1.0; //a throttle so that we can adjust badSteakMax
 
 	for (int i = 1; i < 10000000; i++) {
 		system.trial(); //do a trial
@@ -50,23 +50,23 @@ int main(int argc, char ** argv) {
 		if (badStreak == 0) {
 			cout << i << " e=" << system.totalEnergy << endl;
 			badStreakThrottle = badStreakThrottle > 1.1 ? badStreakThrottle / 1.01 : 1.0;
-			cluster.put(to_string(machineID),system.toString()); //save this result to the cluster
+			cluster.put(to_string(machineID),system.toString()); //save this workers state to the cluster. They key is the machineID.
 		}
 
 		//if we hit the badStreakMax number of failures, Then grab a new state from the cluster.
 		else if (badStreak > badStreakMax * badStreakThrottle) {
-			vector<string> keys = cluster.getKeys();
-			for(size_t i=0;i<keys.size();i++)
+			vector<string> keys = cluster.getKeys(); //get a list of all the workerIDs from the cluster
+			for(size_t i=0;i<keys.size();i++) //for each workerID in the cluster
 			{
 				PointList t;
-				t.fromString(cluster.get(keys.at(i)));
-				if(t.totalEnergy < system.totalEnergy)
+				t.fromString(cluster.get(keys.at(i))); //create a new system from the workerID's state
+				if(t.totalEnergy < system.totalEnergy) //is is better then what I currently have?
 				{
-					cout << "replacing this state e=" << system.totalEnergy << " with workerID=" << keys.at(i) << "'s state e=" << t.totalEnergy << endl;
+					cout << i << "replacing this state e=" << system.totalEnergy << " with workerID=" << keys.at(i) << "'s state e=" << t.totalEnergy << endl;
 					system.fromString(t.toString());
 				}
 			}
-			badStreak=0;
+			badStreak=0; //reset our counter
 		}
 		prevTotalEnergy = system.totalEnergy;
 	}
